fa90c42ee67b789149c29c6afa19dcff1a65042a
Ian Nugteren <99045693@mydavinci.nl>
2017-12-20 10:50:11 +0100

    added first letter random word

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 var random = Math.floor(Math.random() * 479);
 var answer = words[random];
 var answerArray = answer.split('');
+var checkButton = document.getElementById('check');
 console.log(answerArray);
 
-var checkButton = document.getElementById('check');
+function Start() {
+    var res = answer.charAt(0)
+    document.getElementById("letter_0").value = res;
+}
 
 checkButton.setAttribute ("onclick", "check()");
 
@@ -12,10 +16,18 @@ function check(){
         var input = document.getElementById('letter_' + i);
         console.log(input.value);
 
+        // Letters worden rood als ze op de goede plaats staan.
+
         if(input.value == answerArray[i]){
             input.style.backgroundColor = 'red';    
         }
+
+        // Letter die in het woord maar niet op de juiste plek staan worden geel.
+
+
     }
 }
+
+
 
 
